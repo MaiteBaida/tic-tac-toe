96f3e6f97d86ec2574ef9894465c87f69554579d
Maite Baida <maitemsabaida@gmail.com>
2024-04-10 14:18:24 -0600

    grid styles initial

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import "./styles/_global.scss";
+import Grid from "./components/Grid";
+
 function App() {
   return (
     <div className="App">
       <h1>Tic-Tac-Toe</h1>
+      <Grid />
     </div>
   );
 }
---
+++ src/components/Grid.js
@@ -0,0 +1,21 @@
+import "./Grid.scss";
+
+function Grid() {
+  return (
+    <main className="main">
+      <div className="grid-container">
+        <div className="grid-item g11"></div>
+        <div className="grid-item g12"></div>
+        <div className="grid-item g13"></div>
+        <div className="grid-item g21"></div>
+        <div className="grid-item g22"></div>
+        <div className="grid-item g23"></div>
+        <div className="grid-item g31"></div>
+        <div className="grid-item g32"></div>
+        <div className="grid-item g33"></div>
+      </div>
+    </main>
+  );
+}
+
+export default Grid;
